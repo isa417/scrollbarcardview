58cf476dd472593ddd061c16311ad1e2b29f1b19
isa417 <isabellaCaTe417@micorreo.upp.edu.mx>
2024-04-01 17:37:31 -0600

    seguridad terminado y actualizado

--- app/src/main/java/com/example/scrollbarcardview/menu_inicio.java
+++ app/src/main/java/com/example/scrollbarcardview/menu_inicio.java
@@ -10,9 +10,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class menu_inicio extends AppCompatActivity {
-
     ImageView seguridad;
 
     @Override
@@ -20,22 +23,28 @@ public class menu_inicio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu_inicio);
+
+        seguridad = findViewById(R.id.seguridad);
+        seguridad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(menu_inicio.this, drawviewseg.class);
+                startActivity(i);
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        seguridad = (ImageView) findViewById(R.id.seguridad);
-        seguridad.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                Intent i = new Intent(menu_inicio.this, drawviewseg.class);
-                startActivity(i);
-
-            }
-        });
     }
 }
+
+
+
+
+
+
+
